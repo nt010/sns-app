7c9e36d2fd3a555723707bdf9869f36339f285aa
nt <tomnkmr0120@gmail.com>
2024-08-14 00:32:23 +0900

    supabase & auth

---
+++ src/repositories/auth.js
@@ -0,0 +1,21 @@
+import { supabase } from "../lib/supabase";
+
+export const authRepository = {
+  async signup(name, email, password) {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: { data: { name } },
+    });
+    if (error != null) throw new Error(error.message);
+    return { ...data.user, userName: data.user.user_metadata.name };
+  },
+  async signin(email, password) {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error != null) throw new Error(error.message);
+    return { ...data.user, userName: data.user.user_metadata.name };
+  },
+};
